d5dbecba3ce124905e3c629be22f88d47e521c05
Jamie Ahmed <jamieahmed273@gmail.com>
2022-07-14 20:54:03 -0400

    add login,sign up,Navbar and new css file for Navbar and   edit landing page  and app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
-import Signup from './pages/Signup/Signup'
-import Login from './pages/Login/Login'
-import Landing from './pages/Landing/Landing'
-import Profiles from './pages/Profiles/Profiles'
-import ChangePassword from './pages/ChangePassword/ChangePassword'
-import * as authService from './services/authService'
+import { useState } from "react";
+import './App.css'
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Landing from "./pages/Landing/Landing";
+import Profiles from "./pages/Profiles/Profiles";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import * as authService from "./services/authService";
 
 const App = () => {
-  const [user, setUser] = useState(authService.getUser())
-  const navigate = useNavigate()
+  const [user, setUser] = useState(authService.getUser());
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    authService.logout()
-    setUser(null)
-    navigate('/')
-  }
+    authService.logout();
+    setUser(null);
+    navigate("/");
+  };
 
   const handleSignupOrLogin = () => {
-    setUser(authService.getUser())
-  }
+    setUser(authService.getUser());
+  };
 
   return (
     <>
@@ -51,7 +52,7 @@ const App = () => {
         />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,23 @@
-import styles from './Landing.module.css'
+import styles from "./Landing.module.css";
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
-    </main>
-  )
-}
+    <>
+      {user ? (
+        <main className={styles.container}>
+          <li>Hello, {user.name},This is your main page</li>
+        </main>
+      ) : (
+        <>
+          <h1>Well come to Squad Swap</h1>
+          <p>Introduction to our website should be here</p>
+          <div className="footer">
+            <p>Copy write 2022 Squad Swap</p>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
