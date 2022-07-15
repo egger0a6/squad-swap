575fc45fed334249f7906c94f93c02421b95afa3
RazorHollow <rob@razorhollow.com>
2022-07-15 11:34:06 -0400

    add categories data file

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -1,39 +1,76 @@
-import { useState } from "react";
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { TextField } from '@mui/material';
 
-function AddPost(props) {
+const AddPost = () => {
+  const [age, setAge] = useState('');
 
-  const [itemCondition, setItemCondtion] = useState(["New", "Open Box", "Used (normal wear)", "Rough!"])
+  const [formData, setFormData] = useState({
+    image: '',
+    title: '',
+    price: "",
+    category: '',
+    description: '',
+    condition: '',
+    tags: [],
+  })
 
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  };
+  
+  const itemCondition = ["New", "Open Box", "Used (normal wear)", "Rough!"]
+ 
+  
   return (
-    <>
-      <h1>Post an Item</h1>
-      <form autoComplete="off">
-        <div>
-          <label htmlFor="name-input" className="form-label">
-            Title
-          </label>
-          <input
-            type='text'
-            className="form-control"
-            id="name-input"
-            name="name"
-            required
+    <Box sx={{ minWidth: 120 }} component="form" >
+      <FormControl variant="filled" fullWidth >
+        <TextField id="filled-basic" label="Title" variant="filled" name='title' onChange={handleChange} />
+        </FormControl>
+      <FormControl variant="filled" fullWidth >
+        <TextField
+          id="item-desciption"
+          label="Description"
+          multiline
+          rows={4}
+          defaultValue=""
+          variant="filled"
+          name='description' 
+          onChange={handleChange}
           />
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <textarea
-            className="form-control"
-            id="description"
-            name="description"
-            rows="3"
-            required
-          />
-          
-        </div>
-      </form>
-    </>
-  )
+      </FormControl>
+      <FormControl variant="filled" fullWidth>
+        <InputLabel id="demo-simple-select-label">Condition</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={formData.condition}
+          label="Condition"
+          onChange={handleChange}
+          name="condition"
+        >
+          {itemCondition.map((condition, idx) => (
+            <MenuItem value={condition} key={idx}>{condition}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField 
+      inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} 
+      name="price"
+      type="number"
+      min="0"
+      label="Price"
+      variant='filled'
+      fullWidth
+      onChange={handleChange} />
+
+    </Box>
+  );
 }
 
 export default AddPost
+
