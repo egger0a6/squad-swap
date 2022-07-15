f230b7a31d283b0c48510952528c7dbec1271bf5
Jamie Ahmed <jamieahmed273@gmail.com>
2022-07-15 15:32:43 -0400

    edit index gallery

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,13 @@
 import styles from "./Landing.module.css";
 import BottomNav from "../../components/NavBar/BottomNav";
-
-const Landing = ({ user }) => {
+import PostGallery from "../../components/PostGallery/PostGallery";
+const Landing = ({ user, posts }) => {
   return (
     <>
       {user ? (
         <main className={styles.container}>
           <h1>This is Home page,</h1>
+          <PostGallery posts={posts} />
           <BottomNav />
         </main>
       ) : (
