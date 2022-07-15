bbd7fb5e76c3e809a6b98e9ffe81fefd018a231f
RazorHollow <rob@razorhollow.com>
2022-07-15 16:43:08 -0400

    pass state to post card

--- src/pages/ShowPost/ShowPost.jsx
+++ src/pages/ShowPost/ShowPost.jsx
@@ -4,8 +4,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Link, useLocation } from 'react-router-dom'
 
-export default function MediaCard({ post }) {
+
+export default function ShowPost() {
+const location = useLocation()
+const post = location.state.post
+console.log(post)
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -16,7 +22,7 @@ export default function MediaCard({ post }) {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {post.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
