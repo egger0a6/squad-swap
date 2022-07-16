68e7ae48b54b97f1e82ddbb257efbfaa8edf0ef4
RazorHollow <rob@razorhollow.com>
2022-07-16 16:43:23 -0400

    Add post details to show card and link to owner profile

--- src/pages/ShowPost/ShowPost.jsx
+++ src/pages/ShowPost/ShowPost.jsx
@@ -4,7 +4,12 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, NavLink } from 'react-router-dom'
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 
 export default function ShowPost() {
@@ -13,26 +18,75 @@ const post = location.state.post
 console.log(post)
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {post.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}>
+
+        <Grid item xs={3}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://picsum.photos/500/450"//HARD CODED change to ->{post.image}
+              alt="item picture"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {post.title}
+              </Typography>
+              <Typography variant="h6" color="text.secondary">
+                $ {post.price}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                Condition: {post.condition}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary">
+                Category: {post.category}
+              </Typography>
+              <Divider variant="middle"/>
+              <NavLink 
+                to="/"
+                sx={{
+                }} >
+                <Box
+                  sx={{
+                    display : 'flex',
+                    textDecoration: 'none',
+                    wrap: 'no-wrap',
+                    margin: '1em',
+                    alignItems: 'center'
+                  }}>
+                  <Avatar src="/broken-image.jpg" />
+                  <Box
+                  sx={{
+                    flexDirection: 'column',
+                    marginLeft: '1em'
+                  }}
+                  >
+                    <Typography variant='body1'>{post.owner.name}</Typography>
+                    <Typography variant='body1'>Member Since {post.owner.createdAt.slice(0, 4)}</Typography>  
+                  </Box>  
+                  <ChevronRightIcon />
+                </Box>
+              </NavLink>
+              <Divider variant="middle" />
+              <Typography variant="h6" color="text.secondary">
+                Description
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                {post.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Ask Question</Button>
+              <Button size="small">Make Offer</Button>
+            </CardActions>
+          </Card>
+      </Grid>
+    </Grid>
   );
 }
