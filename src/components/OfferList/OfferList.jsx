a7bc45ae4f894d65fb47795afc22d9f8a9d3ad5e
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-20 11:12:43 -0400

    add information to OfferList

--- src/components/OfferList/OfferList.jsx
+++ src/components/OfferList/OfferList.jsx
@@ -4,8 +4,12 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import SellIcon from '@mui/icons-material/Sell';
+import Typography from '@mui/material/Typography';
+import { Fragment } from 'react';
+
 
 export default function OfferList({offers}) {
+  console.log(offers)
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
       {offers.map(offer => 
@@ -15,7 +19,22 @@ export default function OfferList({offers}) {
               <SellIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary={`$${offer.price}`} secondary={offer.createdAt} />
+          <ListItemText 
+            primary={`$${offer.price}`}
+            secondary={
+              <Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {offer.owner.name}
+                </Typography>
+                {` — ${new Date(offer.createdAt).toDateString()}`}
+              </Fragment>
+            }
+          />
         </ListItem>
       )}
     </List>
