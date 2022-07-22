175187f10ee95a8851b928c89d5139b0d35e4390
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-22 01:37:26 -0400

    add styling to landing image gallery

--- src/components/PostGallery/PostGallery.jsx
+++ src/components/PostGallery/PostGallery.jsx
@@ -1,25 +1,28 @@
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import Box from '@mui/material/Box';
 import { Link } from 'react-router-dom';
 
 
 export default function PostGallery({ posts }) {
   return (
-    <ImageList sx={{ width: "100%", height: "90%" }} cols={3} rowHeight={164}>
-      {posts.map((post) => (
-        <ImageListItem key={post._id} component={Link} to={`/${post._id}`}>
-          <img
-            src={
-              post?.photo
-              ? post.photo
-              : "../image-placeholder.jpg"
-            }
-            srcSet={post.photo}
-            alt={post.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Box sx={{ width: "95vw", height: "100vh", overflowY: 'scroll' }}>
+      <ImageList cols={3} gap={9}>
+        {posts.map((post) => (
+          <ImageListItem key={post._id} component={Link} to={`/${post._id}`}>
+            <img
+              src={
+                post?.photo
+                ? `${post.photo}?w=164&h=164&fit=crop&auto=format`
+                : "../image-placeholder.jpg"
+              }
+              srcSet={post.photo}
+              alt={post.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
   );
 }
